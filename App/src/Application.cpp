d151fb46ad62e6dd82ee6b26536547dca56607a6
Adamska-01 <silverworld@hotmail.it>
2025-07-06 18:00:28 +0200

    Removed example classes

--- App/src/Application.cpp
+++ App/src/Application.cpp
@@ -1,11 +1,7 @@
 #include "Application.h"
-#include "Scenes/ExampleScene.h"
 #include <SubSystems/Window.h>
 
 
 Application::Application()
 {
-	Window::SetWindowTitle("Hello World!");
-
-	SceneManager::LoadScene<ExampleScene>();
 }
